3e5632e55875e2bd5d1941675a30836af3249900
鹿角龍 <ok2875814302@gmail.com>
2025-11-27 20:41:07 +0800

    11.27 关卡进度等级推进

--- Def-Breakout/game/managers/game_manager.lua
+++ Def-Breakout/game/managers/game_manager.lua
@@ -4,7 +4,8 @@ local M = {}
 M.selected_player = 1     -- 存储玩家选择的索引 (1: 普通, 2: 大, 3: 小)
 M.game_state = 'title_state'    -- 游戏默认状态
 M.player_score = 0              -- 游戏结算分数
-M.bricks = {}
+M.brick_count = 0
+M.level = 1
 
 function M.get_state()
     return M.game_state
@@ -22,8 +23,24 @@ function M.set_score(new_score)
     M.player_score = new_score
 end
 
+function M.brick_destoryed()
+    M.brick_count = M.brick_count - 1
+end
+
+function M.get_brick_count()
+    return  M.brick_count
+end
+
 function M.set_brick_count(brick_count)
-    M.player_score = new_score
+    M.brick_count = brick_count
+end
+
+function M.get_level()
+    return M.level
+end
+
+function M.set_level()
+    M.level = M.level + 1
 end
 
 return M
