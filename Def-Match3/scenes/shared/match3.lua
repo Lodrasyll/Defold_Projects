7e3663c8e27e7c2ae72e7f482658b6eda779212b
鹿角龍 <ok2875814302@gmail.com>
2025-12-09 17:54:16 +0800

    12.9 实现了一个最简易的表现层逻辑，抛开流程控制的，使用计时器硬编码的下落与填充动画

--- Def-Match3/scenes/shared/match3.lua
+++ Def-Match3/scenes/shared/match3.lua
@@ -75,36 +75,6 @@ function M.create_block(board, x, y, color)
     }
 end
 
-function M.screen_to_slot(board_origin, screen_x, screen_y)
-    local x = math.floor((screen_x - board_origin.x) / M.block_size)
-    local y = math.floor((screen_y - board_origin.y) / M.block_size)
-    return x, y
-end
-
-function M.slot_to_screen(slot_x, slot_y)
-    local x = (M.block_size / 2) + M.block_size * slot_x
-    local y = (M.block_size / 2) + M.block_size * slot_y
-    return vmath.vector3(x, y, 0.5)
-end
-
-function M.on_board(x, y)
-    return x >= 0 and x < M.board_width and
-        y >= 0 and y < M.board_height
-end
-
-function M.is_neighbor(x1, y1, x2, y2)
-    local diff_x = math.abs(x1 - x2)
-    local diff_y = math.abs(y1 - y2)
-    return (diff_x + diff_y) == 1
-end
-
-function M.is_empty(x, y)
-    if M.on_board(x, y) then
-        return M.board.slot[x][y] == nil
-    end
-    return false
-end
-
 function M.swap_block(x1, y1, x2, y2, on_complete)
     local board = M.board
 
@@ -256,7 +226,7 @@ function M.collapse()
             local block = board.slot[x][read_y]
 
             if block then
-                -- 如果读取位置 != 写入位置，说明方块下面有空洞，需要下落
+                -- 如果读取位置 不等于= 写入位置，说明方块下面有空洞，需要下落
                 if read_y ~= write_y then
                     -- 数据层置换，然后置空
                     board.slot[x][write_y] = block
@@ -305,150 +275,180 @@ function M.fill_board()
     return spawn_list
 end
 
-function M.calculated_match()
-    local board = game_data.board
-    M.matches = {}
-    local match_num = 1
-
-    -- 水平方向检测匹配
-    for y = 0, board_height - 1 do
-        local color_match = nil
-        if board[0][y] then
-            color_match = board[0][y].color
-        end
-        match_num = 1
-
-        for x = 1, board_width - 1 do
-            local current_brick = board[x][y]
-            local current_color = nil
-            if current_brick ~= nil then
-                current_color = current_brick.color
-            end
-
-            if color_match ~= nil and current_color ~= nil and
-            current_color == color_match then
-                match_num = match_num + 1
-            else
-                color_match = board[x][y].color
-
-                if match_num >= 3 then
-                    local match = {}
-                    for x2 = x - 1, x - match_num, -1 do
-                        table.insert(match, board[x2][y])
-                    end
-
-                    table.insert(M.matches, match)
-                end
-
-                color_match = current_color
-                match_num = 1
-            end
-        end
-        -- 边界问题，当最后一个依然为匹配项的时候
-        if match_num >= 3 then
-            local match = {}
-            for x = board_width - 1, board_width - match_num, -1 do
-                table.insert(match, board[x][y])
-            end
-            table.insert(M.matches, match)
-        end
-    end
-
-    -- 垂直方向检测匹配
-    for x = 0, board_width - 1 do
-        local color_match = nil
-        if board[x][0] then
-            color_match = board[x][0].color
-        end
-
-        match_num = 1
-
-        for y = 1, board_height - 1 do
-            local current_brick = board[x][y]
-            local current_color = nil
-            if current_brick ~= nil then
-                current_color = current_brick.color
-            end
-            if color_match ~= nil and current_color ~= nil and
-            current_color == color_match then
-                match_num = match_num + 1
-            else
-                color_match = board[x][y].color
-
-                if match_num >= 3 then
-                    local match = {}
-                    for y2 = y - 1, y - match_num, -1 do
-                        table.insert(match, board[x][y2])
-                    end
-
-                    table.insert(M.matches, match)
-                end
-
-                color_match = current_color
-                match_num = 1
-            end
-        end
-        -- 边界问题，当最后一个依然为匹配项的时候
-        if match_num >= 3 then
-            local match = {}
-            for y = board_height - 1, board_height - match_num, -1 do
-                table.insert(match, board[x][y])
-            end
-            table.insert(M.matches, match)
-        end
-    end
-
-    return #M.matches > 0 and M.matches or false
+function M.screen_to_slot(board_origin, screen_x, screen_y)
+    local x = math.floor((screen_x - board_origin.x) / M.block_size)
+    local y = math.floor((screen_y - board_origin.y) / M.block_size)
+    return x, y
 end
 
-function M.get_falling_brick(factory_url)
-    local board = game_data.board
+function M.slot_to_screen(slot_x, slot_y)
+    local x = (M.block_size / 2) + M.block_size * slot_x
+    local y = (M.block_size / 2) + M.block_size * slot_y
+    return vmath.vector3(x, y, 0.5)
+end
+
+function M.on_board(x, y)
+    return x >= 0 and x < M.board_width and
+        y >= 0 and y < M.board_height
+end
+
+function M.is_neighbor(x1, y1, x2, y2)
+    local diff_x = math.abs(x1 - x2)
+    local diff_y = math.abs(y1 - y2)
+    return (diff_x + diff_y) == 1
+end
+
+function M.is_empty(x, y)
+    if M.on_board(x, y) then
+        return M.board.slot[x][y] == nil
+    end
+    return false
+end
+
+-- function M.calculated_match()
+--     local board = game_data.board
+--     M.matches = {}
+--     local match_num = 1
+
+--     -- 水平方向检测匹配
+--     for y = 0, board_height - 1 do
+--         local color_match = nil
+--         if board[0][y] then
+--             color_match = board[0][y].color
+--         end
+--         match_num = 1
+
+--         for x = 1, board_width - 1 do
+--             local current_brick = board[x][y]
+--             local current_color = nil
+--             if current_brick ~= nil then
+--                 current_color = current_brick.color
+--             end
+
+--             if color_match ~= nil and current_color ~= nil and
+--             current_color == color_match then
+--                 match_num = match_num + 1
+--             else
+--                 color_match = board[x][y].color
+
+--                 if match_num >= 3 then
+--                     local match = {}
+--                     for x2 = x - 1, x - match_num, -1 do
+--                         table.insert(match, board[x2][y])
+--                     end
+
+--                     table.insert(M.matches, match)
+--                 end
+
+--                 color_match = current_color
+--                 match_num = 1
+--             end
+--         end
+--         -- 边界问题，当最后一个依然为匹配项的时候
+--         if match_num >= 3 then
+--             local match = {}
+--             for x = board_width - 1, board_width - match_num, -1 do
+--                 table.insert(match, board[x][y])
+--             end
+--             table.insert(M.matches, match)
+--         end
+--     end
+
+--     -- 垂直方向检测匹配
+--     for x = 0, board_width - 1 do
+--         local color_match = nil
+--         if board[x][0] then
+--             color_match = board[x][0].color
+--         end
+
+--         match_num = 1
+
+--         for y = 1, board_height - 1 do
+--             local current_brick = board[x][y]
+--             local current_color = nil
+--             if current_brick ~= nil then
+--                 current_color = current_brick.color
+--             end
+--             if color_match ~= nil and current_color ~= nil and
+--             current_color == color_match then
+--                 match_num = match_num + 1
+--             else
+--                 color_match = board[x][y].color
+
+--                 if match_num >= 3 then
+--                     local match = {}
+--                     for y2 = y - 1, y - match_num, -1 do
+--                         table.insert(match, board[x][y2])
+--                     end
+
+--                     table.insert(M.matches, match)
+--                 end
+
+--                 color_match = current_color
+--                 match_num = 1
+--             end
+--         end
+--         -- 边界问题，当最后一个依然为匹配项的时候
+--         if match_num >= 3 then
+--             local match = {}
+--             for y = board_height - 1, board_height - match_num, -1 do
+--                 table.insert(match, board[x][y])
+--             end
+--             table.insert(M.matches, match)
+--         end
+--     end
+
+--     return #M.matches > 0 and M.matches or false
+-- end
+
+-- function M.get_falling_brick(factory_url)
+--     local board = game_data.board
     
-    for x = 0, board_width - 1 do
-        local space = false
-        local space_y = -1
-        local y = 0
-        while y <= board_height - 1 do
-            local brick = board[x][y]
-            if space then
-                if brick then
-                    board[x][space_y] = brick
-                    brick.y = space_y
-                    board[x][y] = nil
-                    space = false
-                    y = space_y
-                    space_y = -1
-                elseif brick == nil then
-                    space = true
-                    if space_y == -1 then
-                        space_y = y
-                    end
-                end
-            end
+--     for x = 0, board_width - 1 do
+--         local space = false
+--         local space_y = -1
+--         local y = 0
+--         while y <= board_height - 1 do
+--             local brick = board[x][y]
+--             if space then
+--                 if brick then
+--                     board[x][space_y] = brick
+--                     brick.y = space_y
+--                     board[x][y] = nil
+--                     space = false
+--                     y = space_y
+--                     space_y = -1
+--                 elseif brick == nil then
+--                     space = true
+--                     if space_y == -1 then
+--                         space_y = y
+--                     end
+--                 end
+--             end
             
-            y = y + 1
-        end
+--             y = y + 1
+--         end
         
-    end
+--     end
 
-    for x = 0, board_width - 1 do
-        for y = board_height - 1, 0, -1 do
-            local brick = board[x][y]
-            if not brick then
-                local color = game_data.colors_list[math.random(#game_data.colors_list)]
-                local slot_index = { x, y }
-                local pos = utils.board_slot_to_world(slot_index)
-                local new_id = factory.create(factory_url, pos, nil, { color = hash(color) })
-                board[x][y] = {
-                    id = new_id,
-                    x = x,
-                    y = y,
-                    color = hash(color),
-                    pos = pos
-                }
-            end
-        end
-    end
-end
+--     for x = 0, board_width - 1 do
+--         for y = board_height - 1, 0, -1 do
+--             local brick = board[x][y]
+--             if not brick then
+--                 local color = game_data.colors_list[math.random(#game_data.colors_list)]
+--                 local slot_index = { x, y }
+--                 local pos = utils.board_slot_to_world(slot_index)
+--                 local new_id = factory.create(factory_url, pos, nil, { color = hash(color) })
+--                 board[x][y] = {
+--                     id = new_id,
+--                     x = x,
+--                     y = y,
+--                     color = hash(color),
+--                     pos = pos
+--                 }
+--             end
+--         end
+--     end
+-- end
 
 return M
