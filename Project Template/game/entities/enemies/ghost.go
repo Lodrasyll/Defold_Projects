ff7b41b2e647ace4ab28e8a5f0ec301fe6fb9844
鹿角龍 <ok2875814302@gmail.com>
2026-01-19 16:45:19 +0800

    1.19 新建测试项目和完善项目模版工具

---
+++ Project Template/game/entities/enemies/ghost.go
@@ -0,0 +1,48 @@
+components {
+  id: "enemy"
+  component: "/game/entities/enemies/enemy.script"
+  properties {
+    id: "enemy_type"
+    value: "ghost"
+    type: PROPERTY_TYPE_HASH
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"ghost_down\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/textures/generated/enemies.tilesource\"\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"enemies\"\n"
+  "mask: \"wall\"\n"
+  "mask: \"player\"\n"
+  "mask: \"sword\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      y: 2.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 5.608741\n"
+  "  data: 5.9472256\n"
+  "  data: 10.0\n"
+  "}\n"
+  ""
+}
